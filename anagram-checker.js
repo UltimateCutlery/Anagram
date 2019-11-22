bdfd685c42845b1381926054ae43b36cc0e528a6
UltimateCutlery <57415618+UltimateCutlery@users.noreply.github.com>
2019-11-22 22:43:35 +0000

    anagram_attempt1

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,3 +1,20 @@
 
 // More Information about this assisngment is available in the README.md file
 
+
+function checkAnagram(word1, word2){
+    word1 = word1.replace(/\s+/g, '');
+    word2 = word2.replace(/\s+/g, '');
+    var chars1 = word1.split("").sort();
+    var chars2 = word2.split("").sort();
+    var letters1 = chars1.toString();
+    var letters2 = chars2.toString();
+
+    if (word1 === word2) 
+        return false
+    if (letters1 === letters2)
+        return true
+    else 
+        return false
+
+}
